247f52c0744fadf761d6c0d0799466c8fa083b2b
namln2k <namln2aug2k@gmail.com>
2026-02-03 17:14:00 +0700

    fix: user roles seeding to database

--- src/common/constants/roles.constants.ts
+++ src/common/constants/roles.constants.ts
@@ -2,4 +2,5 @@ export enum UserRole {
   ADMIN = 'admin',
   MODERATOR = 'moderator',
   USER = 'user',
+  GUEST = 'guest'
 }
--- src/database/seeds/role.seeder.ts
+++ src/database/seeds/role.seeder.ts
@@ -1,12 +1,13 @@
 import { DataSource } from 'typeorm';
 import { Seeder } from 'typeorm-extension';
 import { Role } from '../../auth/entities/role.entity';
+import { UserRole } from 'src/common/constants/roles.constants';
 
 const roles = [
-  { id: '8cdf8858-2b0b-4192-8016-1de183493664', name: 'admin' },
-  { id: 'f87700b3-58dd-40b1-985f-4e14ae926570', name: 'moderator' },
-  { id: '5e5cfaa9-fc50-4bfe-bdbe-f29b81413437', name: 'user' },
-  { id: '1f332442-d867-4cd6-83eb-cfa351779ce1', name: 'guest' },
+  { id: '8cdf8858-2b0b-4192-8016-1de183493664', name: UserRole.ADMIN },
+  { id: 'f87700b3-58dd-40b1-985f-4e14ae926570', name: UserRole.MODERATOR },
+  { id: '5e5cfaa9-fc50-4bfe-bdbe-f29b81413437', name: UserRole.USER },
+  { id: '1f332442-d867-4cd6-83eb-cfa351779ce1', name: UserRole.GUEST },
 ];
 
 export default class UserSeeder implements Seeder {
